a99f7252b17551fc958ad908ae94d22d7a338dd7
hodeiez <hodeinaiz@hotmail.com>
2022-01-27 23:39:34 +0100

    for heroku 6

--- src/main/resources/db/mockdata/insert_users.sql
+++ src/main/resources/db/mockdata/insert_users.sql
@@ -1,8 +1,8 @@
-INSERT INTO public.user (id,username,email,password)
+INSERT INTO public.user (id,username,email,password,verified)
 VALUES
-(1,'hodei','hodei@email.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
-(2,'batman','bat@man.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
-(3, 'wonderwoman','wonder@woman.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
-(4,'frankZappa', 'hot@rats.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
-(5, 'edison','justringme@noneedofemail.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
-(6, 'milton erickson', 'idontknowiknow@mail.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy');
+(1,'hodei','hodei@email.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy',true),
+(2,'batman','bat@man.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy',true),
+(3, 'wonderwoman','wonder@woman.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy',true),
+(4,'frankZappa', 'hot@rats.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy',true),
+(5, 'edison','justringme@noneedofemail.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy',true),
+(6, 'milton erickson', 'idontknowiknow@mail.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy',true);
