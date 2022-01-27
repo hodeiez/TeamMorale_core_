178bc3d1534e3174cd40fc25e2a8a0907bb3aa01
hodeiez <hodeinaiz@hotmail.com>
2022-01-27 23:22:52 +0100

    for heroku 5

--- src/main/resources/db/mockdata/insert_users.sql
+++ src/main/resources/db/mockdata/insert_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO public.user (id,username,email,password)
 VALUES
-(1,'hodei','hodei@email.com','123'),
-(2,'batman','bat@man.com','123'),
-(3, 'wonderwoman','wonder@woman.com','123'),
-(4,'frankZappa', 'hot@rats.com','123'),
-(5, 'edison','justringme@noneedofemail.com','123'),
-(6, 'milton erickson', 'idontknowiknow@mail.com','123');
+(1,'hodei','hodei@email.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
+(2,'batman','bat@man.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
+(3, 'wonderwoman','wonder@woman.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
+(4,'frankZappa', 'hot@rats.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
+(5, 'edison','justringme@noneedofemail.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy'),
+(6, 'milton erickson', 'idontknowiknow@mail.com','$2a$10$Nmu1mznIgWNOdQmReyo47.5eKDZ520AdXCRriStEdvhOU8dYblEHy');
