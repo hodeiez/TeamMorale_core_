eb0c766fb94455667cee7280e16a7f558bd651b4
hodeiez <hodeinaiz@hotmail.com>
2022-02-02 15:29:45 +0100

    added comments for user service

--- src/main/java/hodei/naiz/teammorale/service/UserService.java
+++ src/main/java/hodei/naiz/teammorale/service/UserService.java
@@ -19,6 +19,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 /**
  * Created by Hodei Eceiza
@@ -36,46 +37,76 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final JWTutil jWTutil;
 
-
+    /**
+     * save a user in database and send email for verifying account,will return error if:
+     * id is not null, password is not validate or email is not validate
+     * @param user the user pojo
+     * @return UserResource has username and user email.
+     */
     @Transactional
     public Mono<UserResource> create(User user) {
         if (user.getId() != null)
             return Mono.error(new IllegalArgumentException("Id must be null"));
+        if(!passwordValidator(user.getPassword()))
+            return Mono.error(new IllegalArgumentException("Password format is: at least one digit, one special character,one lower case and one capital case, and 8 characters is minimum length"));
+        if(!emailValidator(user.getEmail()))
+            return Mono.error(new IllegalArgumentException("looks like you didn't wrote an email address"));
         return userRepo.save(user.withPassword(passwordEncoder.encode(user.getPassword())))
                 .map(userMapper::toUserResource)
-                .doOnNext(u->publisherService.sendEmail(EmailServiceMessage.buildSignedUp()
-                                .username(u.getUsername())
-                                .to(u.getEmail())
-                                .confirmationToken("Bearer "+jWTutil.createVerifyAccountToken(user))
-                                .emailType(EmailType.SIGNUP)
-                                .message("Message sent on "+ LocalDateTime.now().toLocalDate())
-                                .build()));
+                .doOnNext(u -> publisherService.sendEmail(EmailServiceMessage.buildSignedUp()
+                        .username(u.getUsername())
+                        .to(u.getEmail())
+                        .confirmationToken("Bearer " + jWTutil.createVerifyAccountToken(user))
+                        .emailType(EmailType.SIGNUP)
+                        .message("Message sent on " + LocalDateTime.now().toLocalDate())
+                        .build()));
 
     }
 
-
+    /**
+     * returns user authority  details(username,email and token) if user is in database,
+     * the input password matches and if user is verfiied
+     * @param userlogin DTO for login
+     * @return UserAuthResource (username,email,token)
+     */
     public Mono<UserAuthResource> login(UserLoginResource userlogin) {
 
         return userRepo.findOneByEmail(userlogin.getEmail())
-                .filter(u-> passwordEncoder.matches(userlogin.getPassword(), u.getPassword())).filter(User::isVerified).switchIfEmpty(Mono.error(new IllegalArgumentException("not verified")))
-               .map(user->userMapper.toUserAuth(user, jWTutil.createTokenWhenLogin(userlogin)));
+                .filter(u -> passwordEncoder.matches(userlogin.getPassword(), u.getPassword())).filter(User::isVerified).switchIfEmpty(Mono.error(new IllegalArgumentException("not verified")))
+                .map(user -> userMapper.toUserAuth(user, jWTutil.createTokenWhenLogin(userlogin)));
 
 
     }
 
+    /**
+     * get user by given email
+     * @param email given email
+     * @return UserResource has username and user email
+     */
     public Mono<UserResource> getByEmail(String email) {
         return userRepo.findOneByEmail(email).map(userMapper::toUserResource);
     }
 
-    public Mono<UserResource> updateMe(String authorization, User user) {
+    /**
+     * updates the user by given email and user.
+     * @param email the user email
+     * @param user the user pojo
+     * @return UserResource has username and user email
+     */
+    public Mono<UserResource> updateMe(String email, User user) {
 
-        return userRepo.findOneByEmail(authorization).flatMap(u -> {
+        return userRepo.findOneByEmail(email).flatMap(u -> {
             u.setUsername(user.getUsername());
             u.setModifiedDate(LocalDateTime.now());
             return userRepo.save(u).map(userMapper::toUserResource);
         });
     }
 
+    /**
+     * gets evaluation averages and max/min of the given user
+     * @param email the email of the user
+     * @return average and max and min values of users evaluations
+     */
     public Mono<UserEvaluationCalculationsResource> getMyEvaluationCalculations(String email) {
 
         return userRepo.findOneByEmail(email).flatMap(u -> userRepo.getEvaluationsTeamAverageByDate(u.getId()).collectList().
@@ -84,40 +115,76 @@ public class UserService {
 
     }
 
+    /**
+     * updates password if user is found and if user sent "old password" matches
+     * @param authorization email of the user, we use this to find the user
+     * @param creds user new password, user old password and user email
+     * @return UserResource, and error if old password doesn't match
+     */
     public Mono<UserResource> changePass(String authorization, UserLoginResource creds) {
 
         return userRepo.findOneByEmail(authorization).flatMap(u -> {
-            if (passwordEncoder.matches(creds.getOldPassword(),u.getPassword())) {
+            if (passwordEncoder.matches(creds.getOldPassword(), u.getPassword())) {
                 u.setPassword(passwordEncoder.encode(creds.getPassword()));
                 return userRepo.save(u).map(userMapper::toUserResource);
             }
             return Mono.error(new IllegalArgumentException("Old password didn't match"));
         });
     }
-    public Mono<String> forgotPass(String email){
-      return  userRepo.findOneByEmail(email).filter(User::isVerified).switchIfEmpty(Mono.error(new IllegalArgumentException("user not found or account not verified")))
-              .doOnSuccess(u->publisherService.sendEmail(EmailServiceMessage.buildForgotPass()
-                      .emailType(EmailType.FORGOT_PASS)
-                      .username(u.getUsername())
-                      .message("follow the instructions")
-                      .confirmationToken(jWTutil.createTokenFromUser(u))
-                      .to(email)
-                      .build()))
-              .map(u->"Instructions sent to " +u.getEmail());
+
+    /**
+     * sends to email service the forgot password email with a token in it
+     * @param email user email
+     * @return string with instruction sent to ... , if email not found or user not verified returns IllegalArgumentException
+     */
+    public Mono<String> forgotPass(String email) {
+        return userRepo.findOneByEmail(email).filter(User::isVerified).switchIfEmpty(Mono.error(new IllegalArgumentException("user not found or account not verified")))
+                .doOnSuccess(u -> publisherService.sendEmail(EmailServiceMessage.buildForgotPass()
+                        .emailType(EmailType.FORGOT_PASS)
+                        .username(u.getUsername())
+                        .message("follow the instructions")
+                        .confirmationToken(jWTutil.createTokenFromUser(u))
+                        .to(email)
+                        .build()))
+                .map(u -> "Instructions sent to " + u.getEmail());
 
     }
-    public Mono<UserResource> resetPass(String resetPassToken,UserLoginResource userLoginResource){
 
-        return jWTutil.validateToken(resetPassToken.substring(7))?userRepo.updatePasswordByEmail(userLoginResource.getEmail(), passwordEncoder.encode(userLoginResource.getPassword()))
-                .map(userMapper::toUserResource):Mono.error(new IllegalArgumentException("error"));
+    /**
+     * updates password after validating received token.
+     * @param resetPassToken jwt token received via email
+     * @param userLoginResource user password and email
+     * @return UserResource has username and user email
+     */
+    public Mono<UserResource> resetPass(String resetPassToken, UserLoginResource userLoginResource) {
+
+        return jWTutil.validateToken(resetPassToken.substring(7)) ? userRepo.updatePasswordByEmail(userLoginResource.getEmail(), passwordEncoder.encode(userLoginResource.getPassword()))
+                .map(userMapper::toUserResource) : Mono.error(new IllegalArgumentException("error"));
     }
-    public Mono<UserResource> verifyAccount(String token){
-       return userRepo.setVerified(jWTutil.getUserEmail(token)).map(userMapper::toUserResource);
+
+    /**
+     * set user verified
+     * @param token given jwt token
+     * @return UserResource has username and user email
+     */
+    public Mono<UserResource> verifyAccount(String token) {
+        return userRepo.setVerified(jWTutil.getUserEmail(token)).map(userMapper::toUserResource);
     }
-    public Mono<Void> deleteMe(String authorization){
+
+    /**
+     * deletes user
+     * @param authorization given jwt token
+     * @return void
+     */
+    public Mono<Void> deleteMe(String authorization) {
         return userRepo.deleteByEmail(jWTutil.getUserEmail(authorization));
     }
 
+    /**
+     *  update method, saved for admin
+     * @param user the user pojo
+     * @return UserResource has username and user email
+     */
     @Transactional
     public Mono<UserResource> update(User user) {
         if (user.getId() != null) {
@@ -133,15 +200,46 @@ public class UserService {
 
     }
 
+    /**
+     * get all users method saved for admin
+     * @return list of userResource
+     */
     public Flux<UserResource> getAll() {
         return userRepo.findAll().map(userMapper::toUserResource);
     }
 
+    /**
+     * delete user method saved for admin
+     * @param id user id
+     * @return username and user email
+     */
     @Transactional
     public Mono<UserResource> delete(Long id) {
 
         return userRepo.findById(id).flatMap(u -> userRepo.delete(u).then(Mono.just(u).map(userMapper::toUserResource)));
 
+
+    }
+
+    /**
+     * password validator, matches when the string: has one or more digits,one or more lowercase letter,
+     * one or more uppercase letter,one or more special character, and is at least 8 characters long
+     * @param password the password string to validate
+     * @return true if matches
+     */
+    private boolean passwordValidator(String password){
+        return password.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=\\s-!()¤?*])(?=\\S+$).{8,}");
+    }
+
+    /**
+     * Email validateion using RFC822 compliant regex
+     * @param email the email string to validate
+     * @return true if is valid
+     */
+    private boolean emailValidator(String email){
+        Pattern ptr = Pattern.compile("(?:(?:\\r\\n)?[ \\t])*(?:(?:(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*\\<(?:(?:\\r\\n)?[ \\t])*(?:@(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*(?:,@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*)*:(?:(?:\\r\\n)?[ \\t])*)?(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*\\>(?:(?:\\r\\n)?[ \\t])*)|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*:(?:(?:\\r\\n)?[ \\t])*(?:(?:(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*\\<(?:(?:\\r\\n)?[ \\t])*(?:@(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*(?:,@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*)*:(?:(?:\\r\\n)?[ \\t])*)?(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*\\>(?:(?:\\r\\n)?[ \\t])*)(?:,\\s*(?:(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*|(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)*\\<(?:(?:\\r\\n)?[ \\t])*(?:@(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*(?:,@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*)*:(?:(?:\\r\\n)?[ \\t])*)?(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\"(?:[^\\\"\\r\\\\]|\\\\.|(?:(?:\\r\\n)?[ \\t]))*\"(?:(?:\\r\\n)?[ \\t])*))*@(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*)(?:\\.(?:(?:\\r\\n)?[ \\t])*(?:[^()<>@,;:\\\\\".\\[\\] \\000-\\031]+(?:(?:(?:\\r\\n)?[ \\t])+|\\Z|(?=[\\[\"()<>@,;:\\\\\".\\[\\]]))|\\[([^\\[\\]\\r\\\\]|\\\\.)*\\](?:(?:\\r\\n)?[ \\t])*))*\\>(?:(?:\\r\\n)?[ \\t])*))*)?;\\s*)");
+
+    return ptr.matcher(email).matches();
 
     }
 }
